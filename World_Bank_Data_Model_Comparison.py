870096d4629fd4257e45c3534add446d8fa17c47
Regaip KURT <regkrt@gmail.com>
2019-07-26 01:36:11 +0300

    Add files via upload

--- World_Bank_Data_Model_Comparison.py
+++ World_Bank_Data_Model_Comparison.py
@@ -9,8 +9,9 @@ from sklearn.model_selection import train_test_split
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.optimizers import Adam
+from keras.optimizers import SGD
 
-veri = pd.read_csv("WB.csv") #veri repository içinden indirilmeli!
+veri = pd.read_csv("datas/WB.csv")
 print(veri.head())
 
 x = veri["Date"].values.reshape(-1,1)
@@ -29,8 +30,8 @@ lr = LinearRegression()
 lr.fit(x,y)
 
 y_pred = lr.predict(x)
-
-y_pred_lr = lr.predict([[32]])
+print("Başlangıç: ", lr.intercept_)
+y_pred_lr = lr.predict([[30]])
 print(y_pred_lr)
 
 pf = PolynomialFeatures(degree=2)
@@ -40,9 +41,10 @@ lr.fit(x2, y)
 y_log = lr.predict(x2)
 print(x2)
 
-y_pred_pr = lr.predict([[1,32,1024]])
+y_pred_pr = lr.predict([[1,30,900]])
 
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.33)
+y_train, y_test = y_train.reshape(-1,1), y_test.reshape(-1,1)
 
 model = Sequential()
 model.add(Dense(32, kernel_initializer="glorot_uniform", activation="relu"))
@@ -50,18 +52,27 @@ model.add(Dense(32, activation="tanh"))
 model.add(Dense(30, activation="relu"))
 model.add(Dense(1))
 
-model.compile(batch_size=1, optimizer=Adam(lr=0.001), loss="mse")
-#daha iyi sonuçlar için optimizer ve loss fonksiyonu değiştirilmeli ve optmize edilmelidir!
+model.compile(batch_size=1, optimizer=Adam(lr=0.01), loss="mse")
 model.fit(x_train, y_train, epochs=15000)
-y_pred_ker = model.predict([[32]])
+
+y_pred_ker = model.predict([[30]])
 y_pred_k = model.predict(x)
 
+plt.figure(figsize=(12.8, 10.24), dpi=100, facecolor='w', edgecolor='k')
 plt.scatter(x,y, label="Original Data")
-plt.scatter(32,y_pred_lr, color="purple", label="Linear Predict of 32", marker="X")
-plt.scatter(32,y_pred_pr, color="black", label="Polynomial Predict of 32", marker="X")
-plt.scatter(32,y_pred_ker, color="green", label="Keras NN Predict of 32", marker="X")
+plt.plot(x,y)
+plt.scatter(30,y_pred_lr, color="red", label="Linear Predict Value of 30", marker="X")
+plt.scatter(30,y_pred_pr, color="black", label="Polynomial Predict Value of 30", marker="X")
+plt.scatter(32,y_pred_ker, color="purple", label="Keras NN Predict of 32", marker="X")
 plt.plot(x, y_pred, color="red", label="Linear Regression")
 plt.plot(x, y_log, color="black", label="Polynomial Regression")
-plt.plot(x, y_pred_k, color="green", label="Keras")
+plt.plot(x, y_pred_k, color="purple", label="Keras")
+
+plt.title("Dünya Bankası\nBüyüme Verisi")
+plt.xticks(x, veri["Date"], rotation=90)
+plt.ylabel("Baz Yıl Tabanlı Büyüme Verisi")
+plt.xlabel("TARİH")
 plt.legend()
+
+plt.savefig("Dünya Büyüme Modeli Karşılaştırmalı.png")
 plt.show()
